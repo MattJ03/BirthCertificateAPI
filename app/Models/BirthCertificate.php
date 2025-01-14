c909846afb15330fb2757d528c2e4fb2b97c8cf7
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-01-14 22:10:25 +0000

    initial commit 2

--- app/Models/BirthCertificate.php
+++ app/Models/BirthCertificate.php
@@ -8,6 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class BirthCertificate extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['first_name', 'parents_surname', 'birth_date', 'weight', 'country'];
 }
